b58146e0861714e61d121c5857a0fb202feb5648
Gabriella.E <63784697+bubleygum@users.noreply.github.com>
2023-03-31 08:11:46 +0700

    a

--- sw.js
+++ sw.js
@@ -12,6 +12,48 @@ self.addEventListener('install', function(event) {
     );
     return self.clients.claim();
   });
+
+  //install
+  self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    self.skipWaiting(); //PENTING bila ada versi baru!!
+    event.waitUntil(
+      caches.open('static')
+        .then(function(cache) {
+          console.log('[Service Worker] Precaching App Shell');
+          cache.addAll([
+            '/',
+            '/index.html',
+            '/blog.html',
+            '/contact.html',
+            '/about.html',
+            '/app.js',
+            '/styles.css',
+            '/images/about-header.jpg',
+            '/images/contact-image.jpg',
+            '/images/example-blog01.jpg',
+            '/images/example-blog02.jpg',
+            '/images/example-blog03.jpg',
+            '/images/example-blog04.jpg',
+            '/images/example-blog05.jpg',
+            '/images/example-blog06.jpg',
+            '/images/example-blog07.jpg',
+            '/images/example-work01.jpg',
+            '/images/example-work02.jpg',
+            '/images/example-work03.jpg',
+            '/images/example-work04.jpg',
+            '/images/example-work05.jpg',
+            '/images/example-work06.jpg',
+            '/images/example-work07.jpg',
+            '/images/example-work08.jpg',
+            '/images/example-work09.jpg',
+            'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+          ]);
+        })
+    )
+  });
   
   self.addEventListener('fetch', function(event) {
     event.respondWith(
@@ -56,47 +98,6 @@ self.addEventListener('install', function(event) {
     event.respondWith(fetch(event.request));
   });
   
-  //install
-  self.addEventListener('install', function(event) {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-    self.skipWaiting(); //PENTING bila ada versi baru!!
-    event.waitUntil(
-      caches.open('static')
-        .then(function(cache) {
-          console.log('[Service Worker] Precaching App Shell');
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/blog.html',
-            '/contact.html',
-            '/about.html',
-            '/app.js',
-            '/styles.css',
-            '/images/about-header.jpg',
-            '/images/contact-image.jpg',
-            '/images/example-blog01.jpg',
-            '/images/example-blog02.jpg',
-            '/images/example-blog03.jpg',
-            '/images/example-blog04.jpg',
-            '/images/example-blog05.jpg',
-            '/images/example-blog06.jpg',
-            '/images/example-blog07.jpg',
-            '/images/example-work01.jpg',
-            '/images/example-work02.jpg',
-            '/images/example-work03.jpg',
-            '/images/example-work04.jpg',
-            '/images/example-work05.jpg',
-            '/images/example-work06.jpg',
-            '/images/example-work07.jpg',
-            '/images/example-work08.jpg',
-            '/images/example-work09.jpg',
-            'https://fonts.googleapis.com/css?family=Roboto:400,700',
-            'https://fonts.googleapis.com/icon?family=Material+Icons',
-            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-          ]);
-        })
-    )
-  });
   
 //   //cache then network
 //   self.addEventListener('fetch', function(event) {
