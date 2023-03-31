56fca0f3f50707ff46557af8a96acf72ed60fd8f
Gabriella.E <63784697+bubleygum@users.noreply.github.com>
2023-03-31 09:25:18 +0700

    z

--- app.js
+++
@@ -1,9 +0,0 @@
-navigator.serviceWorker.register('/sw.js');
-
-// var deferredPrompt;
-// window.addEventListener('beforeinstallprompt', function(event) {
-//   console.log('beforeinstallprompt fired');
-//   event.preventDefault();
-//   deferredPrompt = event;
-//   return false;
-// });
